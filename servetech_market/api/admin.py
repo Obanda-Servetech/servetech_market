7975b4a94fd619fc626cf7124f410d8fedf0f427
unknown <stevendbanda@yahoo.comgit config --global user.name>
2025-04-10 04:12:12 +0200

    customising the admin interface

--- servetech_market/api/admin.py
+++ servetech_market/api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+
+# Customizing the admin interface text
+admin.site.site_header = "ServeTech Admin"
+admin.site.site_title = "ServeTech Admin Portal"
+admin.site.index_title = "Welcome to the ServeTech Admin Interface"
